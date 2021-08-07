720044fbfaa662ba7bdd3e0fbeebb17828d05317
rimgo <grigoryang085@gmail.com>
2021-08-07 19:18:35 +0300

    Initial commit

---
+++ src/app.gateway.ts
@@ -0,0 +1,41 @@
+import { Logger } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WsResponse,
+} from '@nestjs/websockets';
+import { WebSocketServer } from '@nestjs/websockets';
+
+@WebSocketGateway(80)
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger: Logger = new Logger('AppGateway');
+  private clientCount = 0;
+  @WebSocketServer() wss;
+
+  afterInit(): any {
+    this.logger.log(`Socket server init...`);
+  }
+
+  handleConnection() {
+    this.clientCount++;
+    this.wss.emit('client-count', {
+      data: this.clientCount,
+    });
+  }
+
+  handleDisconnect() {
+    this.clientCount--;
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    return { event: 'client-count', data: '516' };
+  }
+}
