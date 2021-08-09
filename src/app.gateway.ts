63cb97e6198a71484e136ccd5033e5ba627d04c1
rimgo <grigoryang085@gmail.com>
2021-08-09 04:11:23 +0300

    feat: add join event

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -7,38 +7,55 @@ import {
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
-  WsResponse,
 } from '@nestjs/websockets';
 import { WebSocketServer } from '@nestjs/websockets';
+
+type TClient = {
+  id: string;
+  name: string;
+};
+
+const findClientById = (clients: TClient[], id: string): boolean => {
+  for (const client of clients) {
+    if (client.id === id) return true;
+  }
+  return false;
+};
 
 @WebSocketGateway(3003, { cors: true })
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   private logger: Logger = new Logger('AppGateway');
-  private clientCount = 0;
+  private appClients = [];
   @WebSocketServer() wss;
 
   afterInit(): any {
     this.logger.log(`Socket server init...`);
   }
 
-  handleConnection() {
-    this.clientCount++;
-    this.wss.emit('client-count', {
-      data: this.clientCount,
+  handleConnection(client: Socket) {
+    console.log(`${client.id} connected...`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.appClients = this.appClients.filter((item) => item.id !== client.id);
+    console.log(`${client.id} disconnected...`);
+    this.wss.emit('get-clients', {
+      data: this.appClients,
     });
   }
 
-  handleDisconnect() {
-    this.clientCount--;
-    this.wss.emit('client-count', {
-      data: this.clientCount,
+  @SubscribeMessage('join')
+  handleMessage(client: Socket, name: string) {
+    if (findClientById(this.appClients, client.id)) return;
+    const newClient: TClient = {
+      id: client.id,
+      name,
+    };
+    this.appClients.push(newClient);
+    this.wss.emit('get-clients', {
+      data: this.appClients,
     });
-  }
-
-  @SubscribeMessage('message')
-  handleMessage(client: Socket, text: string): WsResponse<string> {
-    return { event: 'client-count', data: '516' };
   }
 }
